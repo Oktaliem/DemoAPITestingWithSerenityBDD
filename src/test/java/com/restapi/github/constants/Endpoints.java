b195a15fe4e6b78a24142b5fb033dd897c65de44
Oktaliem <surya_diharja@yahoo.co.id>
2019-01-07 15:24:58 +0800

    refactor

--- src/test/java/com/restapi/github/constants/Endpoints.java
+++ src/test/java/com/restapi/github/constants/Endpoints.java
@@ -3,4 +3,5 @@ package com.restapi.github.constants;
 public class Endpoints {
 
     public static final String Void = "payment/void";
+    public static final String Refund = "payment/void";
 }
--- src/test/java/com/restapi/local/constants/Endpoints.java
+++ src/test/java/com/restapi/local/constants/Endpoints.java
@@ -3,4 +3,5 @@ package com.restapi.local.constants;
 public class Endpoints {
 
     public static final String Void = "payment/void";
+    public static final String Refund = "payment/void";
 }
